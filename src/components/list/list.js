c8b52a0fcb559b0a6981ecd760e6436cdd3db7e8
Matheus Monte <msn@certi.org.br>
2018-02-18 18:57:18 -0400

    Add ListItem reference on list compoenent
    Signed-off-by: Matheus Monte <msn@certi.org.br>

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './list.css';
 
+import ListItem from '../list-item/list-item'; 
+
 class List  extends Component{
     render(){
-        return(<div>List Component ---> Render Method</div>);
+        return(<div>List Component ---> Render Method<br/> <ListItem/></div>);
     }
 }
 
