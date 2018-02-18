a6a6a9476f5aee0f24ba62fb5364ea315c1ff60e
Matheus Monte <clark.monte1@gmail.com>
2018-02-18 18:42:22 -0400

    Add gitignore and create a component structure
    Signed-off-by: Matheus Monte <clark.monte1@gmail.com>

---
+++ src/components/list/list.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './list.css';
+
+class List  extends Component{
+    render(){
+        return(<div>List Component ---> Render Method</div>);
+    }
+}
+
+export default List;
