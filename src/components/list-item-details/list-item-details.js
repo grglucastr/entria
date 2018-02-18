280e8b0a769ecff04048a4e744922a525f3377ac
Matheus Monte <clark.monte1@gmail.com>
2018-02-18 18:49:53 -0400

    Add structure on list-item and list-item-details components
    Signed-off-by: Matheus Monte <clark.monte1@gmail.com>

---
+++ src/components/list-item-details/list-item-details.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+class ListItemDetails  extends Component{
+    render(){
+        return(<div>List Item Details Component ---> Render Method</div>);
+    }
+}
+
+export default ListItemDetails;
---
+++ src/components/list-item/list-item.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+class ListItem  extends Component{
+    render(){
+        return(<div>List Item Component ---> Render Method</div>);
+    }
+}
+
+export default ListItem;
