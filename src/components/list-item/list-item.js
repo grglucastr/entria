642438fcc26f3109caa509bd1d1d3746ad4e3b68
Matheus Clark <msn@certi.org.br>
2018-02-18 19:00:20 -0400

    Add Reference list-item-details on list-item component
    Signed-off-by: Matheus Clark <msn@certi.org.br>

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './list-item.css';
 
+import ListItem from '../list-item-details/list-item-details'; 
 class ListItem  extends Component{
     render(){
         return(<div>List Item Component ---> Render Method</div>);
