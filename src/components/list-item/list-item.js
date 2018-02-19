24df2ba9438c8c93fd12e4851997df91a250acaa
Matheus Clark <msn@certi.org.br>
2018-02-18 21:11:54 -0400

    Fix reference on ListItem component
    Signed-off-by: Matheus Clark <msn@certi.org.br>

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './list-item.css';
 
-import ListItem from '../list-item-details/list-item-details'; 
+import ListItemDetails from '../list-item-details/list-item-details'; 
+
 class ListItem  extends Component{
     render(){
-        return(<div>List Item Component ---> Render Method</div>);
+        return(<div>List Item Component ---> Render Method<br/> <ListItemDetails/></div>);
     }
 }
 
