098920af4c47f64a81200e3c9a10bab0677ccae6
Matheus Clark <msn@certi.org.br>
2018-02-20 00:08:36 -0400

    Add handle to click event inside list item
    Signed-off-by: Matheus Clark <msn@certi.org.br>

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -5,8 +5,12 @@ import PropTypes from 'prop-types';
 import ListItemDetails from '../list-item-details/list-item-details'; 
 
 class ListItem  extends Component{
+    handleClick = () => {
+        alert('You clicked a list Item ' +  this.props.title);
+    }
+
     render(){
-        return(<div className='BackgroundListItem'>
+        return(<div className='BackgroundListItem' onClick={this.handleClick}>
                  <p className='ListItemTitle'> {this.props.title} </p>
                  <p className='ListItemContent'> {this.props.content} </p>
                </div>);
